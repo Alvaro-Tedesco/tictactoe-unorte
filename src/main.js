7f6797e489b0c6c9c2720ffd989a7a1952d655ff
Alvaro Tedesco <alv.ted@outlook.com>
2023-09-17 21:21:55 -0300

    build: Adjustments to general configuration files.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,11 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
-import store from './store';
+import Vue from "vue";
+import "./scss/main.scss";
+import axios from "./http";
+import App from "./App.vue";
+import store from "./store";
+import router from "./router";
+
+Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false;
 
@@ -9,4 +13,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount('#app');
+}).$mount("#app");
