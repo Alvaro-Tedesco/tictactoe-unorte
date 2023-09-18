b49bf2839b692904b3090f649e00aef0a11980f6
Alvaro Tedesco <alv.ted@outlook.com>
2023-09-17 21:23:43 -0300

    feat: Adding tailwindcss library.

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js,vue}"],
+
+  theme: {
+    colors: {
+      "primary": "#D9D9D9",
+      "white": "#FFFFFF",
+      "black": "#000000",
+    },
+
+    extend: {},
+  },
+
+  plugins: [],
+}
+
