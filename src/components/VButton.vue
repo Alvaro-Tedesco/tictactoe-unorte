9bbe33539f899db064b1edf07722f50dcdb8003c
Alvaro Tedesco <alv.ted@outlook.com>
2023-09-17 21:30:01 -0300

    feat: Created global button component.

---
+++ src/components/VButton.vue
@@ -0,0 +1,31 @@
+<template>
+  <section class="root-v-button">
+    <button :class="classes" @click="$emit('click')">{{text}}</button>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "VButton",
+
+  components: {},
+
+  props: {
+    text: { type: String, default: "" },
+    classes: { type: String, default: "" },
+  },
+
+  data() {
+    return {}
+  },
+
+  created() {
+  },
+
+  computed: {},
+
+  watch: {},
+
+  methods: {},
+}
+</script>
