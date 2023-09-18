5e095397519ddd28a89665d188fe4177a982a6cf
Alvaro Tedesco <alv.ted@outlook.com>
2023-09-17 21:30:58 -0300

    feat: Created header component for project pages.

---
+++ src/components/HeaderPage.vue
@@ -0,0 +1,40 @@
+<template>
+  <section class="root-header-page bg-primary">
+    <span>LOGO LER</span>
+
+    <span>Logo UNORTE</span>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "HeaderPage",
+
+  components: {},
+
+  props: {},
+
+  data() {
+    return {}
+  },
+
+  created() {
+  },
+
+  computed: {},
+
+  watch: {},
+
+  methods: {},
+}
+</script>
+
+<style lang="scss" scoped>
+.root-header-page {
+  width: 100vw;
+  display: flex;
+  justify-content: space-between;
+  padding-inline: 30px;
+  padding-block: 15px;
+}
+</style>
