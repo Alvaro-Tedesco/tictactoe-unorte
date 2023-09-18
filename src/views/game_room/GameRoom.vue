0a7f8ad1bd1bf9eddc045b5926fd29363a436630
Alvaro Tedesco <alv.ted@outlook.com>
2023-09-17 21:31:44 -0300

    feat: Created game room page.

---
+++ src/views/game_room/GameRoom.vue
@@ -0,0 +1,60 @@
+<template>
+  <section class="root-game-room">
+    <header-page/>
+
+    <main>
+      <div class="text-center">
+        <v-button text="Jogar" classes="p-3 bg-primary rounded-md"
+                  @click="redirectToGame"/>
+      </div>
+    </main>
+
+
+  </section>
+</template>
+
+<script>
+import HeaderPage from "@/components/HeaderPage.vue";
+import VButton from "@/components/VButton.vue";
+
+export default {
+  name: "GameRoom",
+
+  components: {
+    VButton,
+    HeaderPage,
+  },
+
+  props: {},
+
+  data() {
+    return {}
+  },
+
+  created() {
+    console.log("entrei");
+  },
+
+  computed: {},
+
+  watch: {},
+
+  methods: {
+    redirectToGame() {
+      console.log('redirecionar para jogo');
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.root-game-room {
+  main {
+    width: 100vw;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    height: calc(100vh - 54px);
+  }
+}
+</style>
