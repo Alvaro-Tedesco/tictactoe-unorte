c97bb2296478efd7162a24e795a26d398837b82a
Thiago Rizzo <rizzo.17@outlook.com>
2023-09-19 00:41:59 -0300

    refactor: moved loginPage and associates to home directory, to be a child of baseHome.

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,9 +1,7 @@
 import {routes as gameRoom} from "../views/game_room";
-import {routes as login} from "../views/login";
 import {routes as home} from "../views/home";
 
 export default [
   ...gameRoom,
-  ...login,
   ...home,
 ];
