dba945ad290c7c460afd28342a23f60735320a73
Alvaro Tedesco <alv.ted@outlook.com>
2023-09-19 00:30:39 -0300

    feat: Added screen route for testing the board component.

--- src/views/game_room/routes.js
+++ src/views/game_room/routes.js
@@ -4,4 +4,9 @@ export default [
     path: "/jogo/:room_id/:player_id?",
     component: () => import(/* webpackChunkName: "game_room" */ "./GameRoom.vue"),
   },
+  {
+    name: "game_board",
+    path: "/tabuleiro",
+    component: () => import(/* webpackChunkName: "game_board" */ "./TestBoard.vue"),
+  },
 ];
