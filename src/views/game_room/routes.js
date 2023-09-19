7bc7863265ae49142b018c244187215b2b0c47d2
Alvaro Tedesco <alv.ted@outlook.com>
2023-09-19 12:54:32 -0300

    feat: Added root route to separate game pages.

--- src/views/game_room/routes.js
+++ src/views/game_room/routes.js
@@ -1,7 +1,18 @@
 export default [
   {
-    name: "game_room",
-    path: "/jogo/:room_id/:player_id?",
-    component: () => import(/* webpackChunkName: "game_room" */ "./GameRoom.vue"),
+    path: '/jogo',
+    component: () => import(/* webpackChunkName: "game_root" */ "./GameRoot.vue"),
+    children: [
+      {
+        name: "game_room",
+        path: "/sala/:room_id/:player_id?",
+        component: () => import(/* webpackChunkName: "game_room" */ "./GameRoom.vue"),
+      },
+      {
+        name: "game_board",
+        path: "/jogo/:room_id/:player_id",
+        component: () => import(/* webpackChunkName: "game_board" */ "./GameBoard.vue"),
+      },
+    ],
   },
 ];
