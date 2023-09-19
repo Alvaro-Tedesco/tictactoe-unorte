223edfd30aad36b109eda4f5be254fd3651178f2
Thiago Rizzo <rizzo.17@outlook.com>
2023-09-19 00:45:16 -0300

    feat: added functions for the Play and Match buttons.

--- src/views/home/pages/HomePage.vue
+++ src/views/home/pages/HomePage.vue
@@ -1,37 +1,33 @@
 <template>
-  <section>
-    <header-page/>
+  <main class="flex justify-center">
+    <section>
+    </section>
 
-    <main class="flex justify-center">
-      <section>
-      </section>
-
-      <section>
-        <div class="flex flex-col gap-10">
-          <div>
-            <div class="flex justify-center">
-              <span>Robotica</span>
-            </div>
-            <div class="flex justify-center">
-              <span>Colaborativa</span>
-            </div>
+    <section>
+      <div class="flex flex-col gap-10">
+        <div>
+          <div class="flex justify-center">
+            <span>Robotica</span>
           </div>
           <div class="flex justify-center">
-            <h1 class="p-3 bg-primary rounded-md text-5xl w-fit">Jogo da Velha</h1>
-          </div>
-          <div class="flex justify-center">
-            <v-button>Jogar</v-button>
-          </div>
-          <div class="flex justify-center">
-            <v-button>Partidas</v-button>
+            <span>Colaborativa</span>
           </div>
         </div>
-      </section>
+        <div class="flex justify-center">
+          <h1 class="p-3 bg-primary rounded-md text-5xl w-fit">Jogo da Velha</h1>
+        </div>
+        <div class="flex justify-center">
+          <v-button @click="goToLogin">Jogar</v-button>
+        </div>
+        <div class="flex justify-center">
+          <v-button @click="goToMatches">Partidas</v-button>
+        </div>
+      </div>
+    </section>
 
-      <section>
-      </section>
-    </main>
-  </section>
+    <section>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -45,5 +41,18 @@ export default {
     VButton,
     HeaderPage,
   },
+
+  methods: {
+    goToLogin() {
+      this.$router.push({
+        name: 'login',
+        params: {room_id: '1'},
+      });
+    },
+
+    goToMatches() {
+      this.$router.push({name: 'matches'});
+    }
+  }
 }
 </script>
