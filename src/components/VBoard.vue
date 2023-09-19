89677409c274f22606d4387f8ddf2e456bee1a86
Alvaro Tedesco <alv.ted@outlook.com>
2023-09-19 00:28:07 -0300

    feat: Created the game board component and a test page and route.

---
+++ src/components/VBoard.vue
@@ -0,0 +1,82 @@
+<template>
+  <section class="root-v-board">
+    <table>
+      <caption class="hidden">Tabuleiro</caption>
+      <tr class="h-20 bg-primary">
+        <th id="" class="hidden"></th>
+        <td class="w-20 border-l border-t border-b rounded-tl-md hover:bg-extra-light-green" @click="$emit('moviment', 'firstSquare')">
+          <div class="w-full flex justify-center items-center">
+            <img src="../assets/icons/icon_o.svg" alt="Ícone O">
+          </div>
+        </td>
+
+        <td class="w-20 border-l border-t border-b border-r hover:bg-extra-light-green" @click="$emit('moviment', 'secondSquare')">
+          <div class="w-full flex justify-center items-center">
+            <img src="../assets/icons/icon_o.svg" alt="Ícone O">
+          </div>
+        </td>
+
+        <td class="w-20 border-t border-b border-r rounded-tr-md hover:bg-extra-light-green" @click="$emit('moviment', 'thirdSquare')">
+          <div class="w-full flex justify-center items-center">
+            <img src="../assets/icons/icon_o.svg" alt="Ícone O">
+          </div>
+        </td>
+      </tr>
+
+      <tr class="h-20 bg-primary">
+        <td class="w-20 border-l border-b hover:bg-extra-light-green" @click="$emit('moviment', 'fourSquare')">
+          <div class="w-full flex justify-center items-center">
+            <img src="../assets/icons/icon_x.svg" alt="Ícone X">
+          </div>
+        </td>
+
+        <td class="w-20 border-l border-b border-r hover:bg-extra-light-green" @click="$emit('moviment', 'fiveSquare')">
+          <div class="w-full flex justify-center items-center">
+            <img src="../assets/icons/icon_x.svg" alt="Ícone X">
+          </div>
+        </td>
+
+        <td class="w-20 border-b border-r hover:bg-extra-light-green" @click="$emit('moviment', 'sixSquare')">
+          <div class="w-full flex justify-center items-center">
+            <img src="../assets/icons/icon_x.svg" alt="Ícone X">
+          </div>
+        </td>
+      </tr>
+
+      <tr class="h-20 bg-primary">
+        <td class="w-20 border-l border-b border-r rounded-bl-md hover:bg-extra-light-green" @click="$emit('moviment', 'sevenSquare')">
+          <div class="w-full flex justify-center items-center">
+            <img src="../assets/icons/icon_o.svg" alt="Ícone O">
+          </div>
+        </td>
+
+        <td class="w-20 border-b border-r hover:bg-extra-light-green" @click="$emit('moviment', 'eightSquare')">
+          <div class="w-full flex justify-center items-center">
+            <img src="../assets/icons/icon_o.svg" alt="Ícone O">
+          </div>
+        </td>
+
+        <td class="w-20 border-b border-r rounded-br-md hover:bg-extra-light-green" @click="$emit('moviment', 'nineSquare')">
+          <div class="w-full flex justify-center items-center">
+            <img src="../assets/icons/icon_o.svg" alt="Ícone O">
+          </div>
+        </td>
+      </tr>
+    </table>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "VBoard",
+}
+</script>
+
+<style lang="scss" scoped>
+.root-v-board {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
---
+++ src/views/game_room/TestBoard.vue
@@ -0,0 +1,35 @@
+<template>
+  <section class="root-test-board">
+    <v-board @moviment="makeMoviment($event)"/>
+  </section>
+</template>
+
+<script>
+import VBoard from "@/components/VBoard.vue";
+
+export default {
+  name: "TestBoard",
+
+  components: {
+    VBoard,
+  },
+
+  methods: {
+    /**
+     * @param {string} moviment
+     */
+    makeMoviment(moviment) {
+      console.log('movimento no:', moviment)
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.root-test-board {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+</style>
