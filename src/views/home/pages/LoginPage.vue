fbc9ca3e187db3c2d3e02af52233c04483e2ca16
Thiago Rizzo <rizzo.17@outlook.com>
2023-09-19 00:46:38 -0300

    feat: added layout to login page with qrcode.

---
+++ src/views/home/pages/LoginPage.vue
@@ -0,0 +1,59 @@
+<template>
+  <main class="flex justify-around">
+    <section class="py-12">
+      <div class="flex justify-center">
+        <span>JOGADOR 1</span>
+      </div>
+
+      <div class="flex justify-center">
+        <span>LER O QRCODE PARA ENTRAR NA PARTIDA</span>
+      </div>
+
+      <div class="flex justify-center bg-primary p-2 rounded-2xl">
+        <v-qrcode :value="qrcodeValue"/>
+      </div>
+    </section>
+
+    <div class="border border-black mt-5"></div>
+
+    <section class="py-12">
+      <div class="flex justify-center">
+        <span>JOGADOR 2</span>
+      </div>
+
+      <div class="flex justify-center">
+        <span>LER O QRCODE PARA ENTRAR NA PARTIDA</span>
+      </div>
+
+      <div class="flex justify-center bg-primary p-2 rounded-2xl">
+        <v-qrcode :value="qrcodeValue"/>
+      </div>
+    </section>
+  </main>
+</template>
+
+<script>
+import HeaderPage from "../../../components/HeaderPage.vue";
+import VQrcode from "@/components/VQrcode.vue";
+
+export default {
+  name: 'LoginPage',
+
+  components: {
+    VQrcode,
+    HeaderPage,
+  },
+
+  computed: {
+    qrcodeValue() {
+      return 'https://google.com.br';
+    }
+  },
+
+  methods: {
+    backButton() {
+      this.$router.back();
+    }
+  }
+}
+</script>
