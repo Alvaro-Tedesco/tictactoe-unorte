39174f364242bed1841b1385ee8b21fbd105b5e8
Thiago Rizzo <rizzo.17@outlook.com>
2023-09-19 00:44:18 -0300

    feat: created a standard back button, and a props for it.

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,14 +1,36 @@
 <template>
-  <section class="flex justify-between p-1 bg-primary">
-    <img :src="logoLER" alt="Logo LER" class="w-[50px]"/>
+  <section class="flex p-1 bg-primary">
+    <div class="w-1/3">
+      <template v-if="backButton">
+        <div class="w-fit rounded-3xl">
+          <v-button class="shadow drop-shadow" @click="backButton">voltar</v-button>
+        </div>
+      </template>
+    </div>
 
-    <img :src="logoUNORTE" alt="Logo UNORTE" class="w-[50px]"/>
+    <div class="w-1/3 flex justify-center gap-2">
+      <img class="w-[50px]" :src="logoLER" alt="Logo LER"/>
+      <img class="w-[50px]" :src="logoUNORTE" alt="Logo UNORTE"/>
+    </div>
+
+    <div class="w-1/3"></div>
+
   </section>
 </template>
 
 <script>
+import VButton from "@/components/VButton.vue";
+
 export default {
   name: "HeaderPage",
+
+  components: {
+    VButton,
+  },
+
+  props: {
+    backButton: {type: Function, default: null}
+  },
 
   data() {
     return {
