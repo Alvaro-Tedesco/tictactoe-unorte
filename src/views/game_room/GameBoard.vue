fed69fa34ca8ea8245a6804031442ba488746c34
Alvaro Tedesco <alv.ted@outlook.com>
2023-09-21 19:30:52 -0300

    chore: Included the board component and button to exit the game.

--- src/views/game_room/GameBoard.vue
+++ src/views/game_room/GameBoard.vue
@@ -1,22 +1,53 @@
 <template>
   <section class="root-game-board">
-    <div class="w-full pt-4 px-4 flex justify-between">
-      <div class="p-4 border border-primary bg-primary rounded-md" :class="true ? 'shadow-md shadow-green' : 'shadow-none'">
-        <span>NOME DO JOGADOR 1</span>
+    <div class="w-full pt-4 px-4 flex justify-between gap-20 max-[300px]:gap-12">
+      <div class="p-2 border border-primary bg-primary rounded-md"
+           :class="true ? 'shadow-md shadow-green' : 'shadow-none'">
+        <span class="text-sm">NOME DO JOGADOR 1</span>
       </div>
 
-      <div class="p-4 border border-primary bg-primary rounded-md" :class="true ? 'shadow-md shadow-green' : 'shadow-none'">
-        <span>NOME DO JOGADOR 2</span>
+      <div class="p-2 border border-primary bg-primary rounded-md"
+           :class="true ? 'shadow-md shadow-green' : 'shadow-none'">
+        <span class="text-sm">NOME DO JOGADOR 2</span>
       </div>
     </div>
 
+    <div class="w-full mt-20 flex justify-center">
+      <v-board @move="makeMove($event)"/>
+    </div>
 
+    <div class="w-full mt-12 pb-4 flex justify-center items-center">
+      <v-button classes="p-2 rounded-md bg-primary" text="SAIR" @click="backToGameRoom"/>
+    </div>
   </section>
 </template>
 
 <script>
+import VBoard from "@/components/VBoard.vue";
+import VButton from "@/components/VButton.vue";
+
 export default {
   name: "GameBoard",
+
+  components: {
+    VBoard,
+    VButton,
+  },
+
+  methods: {
+    /**
+     * @param {string} position
+     */
+    makeMove(position) {
+      console.log('movimento no:', position);
+      console.log('Implementar requisição de movimento pro back-end');
+    },
+
+    backToGameRoom() {
+      console.log('Implementar requisição de cancelamento da sessão pro back-end');
+      this.$router.push({name: "game_room", params: {room_id: 1, player_id: 1,},});
+    },
+  },
 }
 </script>
 
