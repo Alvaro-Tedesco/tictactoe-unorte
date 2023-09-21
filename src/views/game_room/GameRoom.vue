6227bdadc3c57a8612d550e52552134cdc9449f8
Alvaro Tedesco <alv.ted@outlook.com>
2023-09-21 19:30:15 -0300

    chore: Added loading component to simulate game entry.

--- src/views/game_room/GameRoom.vue
+++ src/views/game_room/GameRoom.vue
@@ -1,26 +1,41 @@
 <template>
   <section class="root-game-room">
-    <v-button text="Jogar" classes="p-3 bg-primary rounded-md"
-              @click="redirectToGame"/>
+    <v-button text="Jogar" classes="p-3 bg-primary rounded-md" @click="redirectToGame"/>
+
+    <template v-if="activeLoading">
+      <v-loading/>
+    </template>
   </section>
 </template>
 
 <script>
-import HeaderPage from "@/components/HeaderPage.vue";
-import VButton from "@/components/VButton.vue";
+import VButton from "../../components/VButton.vue";
+import VLoading from "../../components/VLoading.vue";
+import HeaderPage from "../../components/HeaderPage.vue";
 
 export default {
   name: "GameRoom",
 
   components: {
     VButton,
+    VLoading,
     HeaderPage,
+  },
+
+  data() {
+    return {
+      activeLoading: false,
+    };
   },
 
   methods: {
     redirectToGame() {
+      this.activeLoading = true;
+
       setTimeout(() => {
-        this.$router.push({name: 'game_board', params: {room_id: 1, player_id: 1}});
+        this.$router.push({name: 'game_board', params: {room_id: 1, player_id: 1,},});
+
+        this.activeLoading = false;
       }, 500);
     },
   },
