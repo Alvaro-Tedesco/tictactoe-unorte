9e7edb279a9e33cfb1babbdc19c28b11bb1d7def
Alvaro Tedesco <alv.ted@outlook.com>
2023-09-24 16:33:26 -0300

    refactor: Adjustment of declaration of color properties in Tailwind configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,15 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js,vue}"],
 
   theme: {
     colors: {
-      "primary": "#D9D9D9",
-      "white": "#FFFFFF",
-      "black": "#000000",
-      "green": "#00FF00",
-      "red": "#770000",
-      "blue": "#000077",
+      primary: "#D9D9D9",
+      white: "#FFFFFF",
+      black: "#000000",
+      orange: "#F25835",
+      green: "#00FF00",
+      red: "#770000",
+      blue: "#000077",
       "extra-light-green": "#C0FFEE",
     },
   },
