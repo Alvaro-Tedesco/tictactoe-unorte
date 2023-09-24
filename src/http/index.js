994a0275f8735e07d4feebd1ec639bebbcac1214
Alvaro Tedesco <alv.ted@outlook.com>
2023-09-24 17:13:38 -0300

    feat: Added logic to create services with axios.

--- src/http/index.js
+++ src/http/index.js
@@ -1,7 +1,21 @@
 import axios from "axios";
+import services from "./services";
 
 /** @type {AxiosInstance} */
-const createdAxios = axios.create();
+const createdAxios = axios.create({baseURL: "http://localhost:8080/"});
 
-export default createdAxios;
+const configs = JSON.parse(JSON.stringify(services));
 
+Object.keys(services).forEach((module) => {
+  Object.keys(services[module]).forEach((service) => {
+    services[module][service] = ({params = null, data = null}) => {
+      return createdAxios.request({
+        params,
+        data,
+        ...configs[module][service],
+      });
+    }
+  });
+});
+
+export default services;
