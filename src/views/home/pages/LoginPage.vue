3994b8a2004c9bd64bf1f19c32c9926729a8a62b
Thiago Rizzo <rizzo.17@outlook.com>
2023-09-24 17:44:24 -0300

    feat: created a ResultDialog component that shows a result of the match.
    added the component to LoginPage.

--- src/views/home/pages/LoginPage.vue
+++ src/views/home/pages/LoginPage.vue
@@ -1,47 +1,62 @@
 <template>
-  <main class="flex justify-around">
-    <section class="py-12 gap-5 flex flex-col">
-      <div class="flex justify-center">
-        <span>JOGADOR 1</span>
-      </div>
+  <section>
+    <template v-if="$store.getters.result !== Result.NONE">
+      <result-dialog :result="$store.getters.result"/>
+    </template>
 
-      <div class="flex justify-center">
-        <span>LER O QRCODE PARA ENTRAR NA PARTIDA</span>
-      </div>
+    <main class="flex justify-around">
+      <section class="py-12 gap-5 flex flex-col">
+        <div class="flex justify-center">
+          <span>JOGADOR 1</span>
+        </div>
 
-      <div class="flex justify-center bg-blue p-2 rounded-2xl">
-        <v-qrcode :value="qrcodeP1"/>
-      </div>
-    </section>
+        <div class="flex justify-center">
+          <span>LER O QRCODE PARA ENTRAR NA PARTIDA</span>
+        </div>
 
-    <div class="border border-black mt-5"></div>
+        <div class="flex justify-center bg-blue p-2 rounded-2xl">
+          <v-qrcode :value="qrcodeP1"/>
+        </div>
+      </section>
 
-    <section class="py-12 gap-5 flex flex-col">
-      <div class="flex justify-center">
-        <span>JOGADOR 2</span>
-      </div>
+      <div class="border border-black mt-5"></div>
 
-      <div class="flex justify-center">
-        <span>LER O QRCODE PARA ENTRAR NA PARTIDA</span>
-      </div>
+      <section class="py-12 gap-5 flex flex-col">
+        <div class="flex justify-center">
+          <span>JOGADOR 2</span>
+        </div>
 
-      <div class="flex justify-center bg-red p-2 rounded-2xl">
-        <v-qrcode :value="qrcodeP2"/>
-      </div>
-    </section>
-  </main>
+        <div class="flex justify-center">
+          <span>LER O QRCODE PARA ENTRAR NA PARTIDA</span>
+        </div>
+
+        <div class="flex justify-center bg-red p-2 rounded-2xl">
+          <v-qrcode :value="qrcodeP2"/>
+        </div>
+      </section>
+    </main>
+  </section>
 </template>
 
 <script>
 import HeaderPage from "../../../components/HeaderPage.vue";
 import VQrcode from "../../../components/VQrcode.vue";
+import ResultDialog from "./components/ResultDialog.vue";
+import Result from "../../../enums/Result";
 
 export default {
   name: 'LoginPage',
 
   components: {
+    ResultDialog,
     VQrcode,
     HeaderPage,
+  },
+
+  data() {
+    return {
+      Result,
+    };
   },
 
   computed: {
