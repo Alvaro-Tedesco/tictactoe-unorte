0b2ff3fab5a1cf7532ac585d47c131b1df18a1c5
Alvaro Tedesco <alv.ted@outlook.com>
2023-09-26 18:58:21 -0300

    chore: Removed prefix "/" from the routes of the services: home and game_room.

--- src/views/game_room/services.js
+++ src/views/game_room/services.js
@@ -1,4 +1,4 @@
 export default {
-  move: {method: "put", url: "/sessions/move/:sessionId",},
-  finish: {method: "delete", url: "/sessions/finish/:sessionId",},
+  move: {method: "put", url: "sessions/move/:sessionId",},
+  finish: {method: "delete", url: "sessions/finish/:sessionId",},
 };
--- src/views/home/services.js
+++ src/views/home/services.js
@@ -1,5 +1,5 @@
 export default {
-    create: {method: 'post', url: '/sessions/create'},
-    all: {method: 'get', url: '/sessions/all'},
-    find: {method: 'get', url: '/sessions/'},
+    create: {method: 'post', url: 'sessions/create'},
+    all: {method: 'get', url: 'sessions/all'},
+    find: {method: 'get', url: 'sessions/'},
 };
