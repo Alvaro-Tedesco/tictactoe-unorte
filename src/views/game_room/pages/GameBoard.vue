542ab11d846bc0c34639a03f9c28445542504096
Alvaro Tedesco <alv.ted@outlook.com>
2023-09-26 18:40:38 -0300

    refactor: adjust in services

--- src/views/game_room/pages/GameBoard.vue
+++ src/views/game_room/pages/GameBoard.vue
@@ -2,18 +2,18 @@
   <section class="root-game-board">
     <div class="w-full pt-4 px-4 flex justify-between gap-20 max-[300px]:gap-12">
       <div class="p-2 border border-primary bg-primary rounded-md"
-           :class="true ? 'shadow-md shadow-green' : 'shadow-none'">
-        <span class="text-sm">NOME DO JOGADOR 1</span>
+           :class="$route.params.playerId === Player.PLAYER_1.id ? 'shadow-md shadow-green' : 'shadow-none opacity-60'">
+        <span class="text-sm">JOGADOR 1</span>
       </div>
 
       <div class="p-2 border border-primary bg-primary rounded-md"
-           :class="true ? 'shadow-md shadow-green' : 'shadow-none'">
-        <span class="text-sm">NOME DO JOGADOR 2</span>
+           :class="$route.params.playerId === Player.PLAYER_2.id ? 'shadow-md shadow-green' : 'shadow-none opacity-60'">
+        <span class="text-sm">JOGADOR 2</span>
       </div>
     </div>
 
     <div class="w-full mt-20 flex justify-center">
-      <v-board :movementDone="movementDone" @move="makeMove($event)"/>
+      <v-board :is-second-player="$route.params.playerId === Player.PLAYER_2.id" @position="setPosition($event)"/>
     </div>
 
     <div class="w-full mt-12 pb-4 flex justify-center items-center">
@@ -26,6 +26,8 @@
 import services from "../../../http";
 import VBoard from "../../../components/VBoard.vue";
 import VButton from "../../../components/VButton.vue";
+import Player from "../../../enums/Player";
+import Piece from "../../../enums/Piece";
 
 export default {
   name: "GameBoard",
@@ -37,17 +39,8 @@ export default {
 
   data() {
     return {
-      movementDone: {
-        A1: null,
-        A2: null,
-        A3: null,
-        B1: null,
-        B2: null,
-        B3: null,
-        C1: null,
-        C2: null,
-        C3: null,
-      },
+      Player,
+      Piece,
     };
   },
 
@@ -55,30 +48,38 @@ export default {
     /**
      * @param {string} position
      */
-    makeMove(position) {
-      const piece = this.$route.params.playerId === "1" ? "orange" : "black";
+    setPosition(position) {
       const piece = this.$route.params.playerId === Player.PLAYER_1.id ? Piece.ORANGE.value : Piece.BLACK.value;
 
       services.gameRoom.move({
-        params: {
+        param: {
           sessionId: this.$route.params.sessionId,
         },
         data: {
-          piece: piece.toUpperCase(),
+          piece,
           position,
-          player: this.$route.params.playerId,
+          player: Player.getPlayer(this.$route.params.playerId).value,
         },
       }).then((response) => {
         this.$store.dispatch("setSession", response.data);
       });
-
-      this.movementDone[position] = piece;
     },
 
     backToGameRoom() {
-      services.gameRoom.finish().then((response) => {
-        this.$router.push({name: "game_room", params: {room_id: 1, player_id: 1,},});
-      })
+      services.gameRoom.finish({
+        param: {
+          sessionId: this.$route.params.sessionId,
+        },
+      }).then((response) => {
+        console.log(response);
+        this.$router.push({
+          name: "game_room",
+          params: {
+            sessionId: this.$route.params.sessionId,
+            playerId: this.$route.params.playerId,
+          },
+        });
+      });
     },
   },
 }
