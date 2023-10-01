1c80b803f05c8d3761a9b464e452974d9a57313b
Alvaro Tedesco <alv.ted@outlook.com>
2023-10-01 18:23:33 -0300

    style: Adjusted the layout to the layout defined so far for the match listing page in the colors: orange, black and blue.

--- src/views/home/pages/MatchesPage.vue
+++ src/views/home/pages/MatchesPage.vue
@@ -2,14 +2,29 @@
   <div class="flex justify-center p-3">
     <v-list :list="data"/>
   </div>
+  <section class="w-full h-full p-4">
+    <div class="w-full p-8 flex justify-center">
+      <label class="text-white text-3xl">Controle de Jogos</label>
+    </div>
+
+    <div class="w-full p-4 flex justify-center">
+    </div>
+
+    <div class="w-full pt-6 flex justify-center">
+      <v-button classes="p-2 bg-primary rounded-md text-sm" @click="backToHomePage">Voltar</v-button>
+    </div>
+  </section>
 </template>
+
 <script>
 import VList from "../../../components/VList.vue";
+import VButton from "../../../components/VButton.vue";
 
 export default {
   name: "MatchesPage",
 
   components: {
+    VButton,
     VList,
   },
 
@@ -45,6 +60,12 @@ export default {
           match: 1,
         },
       ];
+
+  methods: {
+    backToHomePage() {
+      this.$router.push({
+        name: "home",
+      });
     },
   },
 }
