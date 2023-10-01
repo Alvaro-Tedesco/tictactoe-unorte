5072630967837ce772b6e9531812d898a69984bc
Alvaro Tedesco <alv.ted@outlook.com>
2023-10-01 18:27:29 -0300

    chore: Adjusted the variable name that contains the data for the match listing and removed fixed code from the listing page.

--- src/views/home/pages/MatchesPage.vue
+++ src/views/home/pages/MatchesPage.vue
@@ -1,13 +1,11 @@
 <template>
-  <div class="flex justify-center p-3">
-    <v-list :list="data"/>
-  </div>
   <section class="w-full h-full p-4">
     <div class="w-full p-8 flex justify-center">
       <label class="text-white text-3xl">Controle de Jogos</label>
     </div>
 
     <div class="w-full p-4 flex justify-center">
+      <v-list :list="matches"/>
     </div>
 
     <div class="w-full pt-6 flex justify-center">
@@ -28,38 +26,11 @@ export default {
     VList,
   },
 
-  computed: {
-    data() {
-      return [
-        {
-          winner: 'Jogador 1',
-          match: 0,
-        },
-        {
-          winner: 'Jogador 2',
-          match: 1,
-        },
-        {
-          winner: 'Jogador 23',
-          match: 1,
-        },
-        {
-          winner: 'Jogador 25',
-          match: 1,
-        },
-        {
-          winner: 'Jogador 452',
-          match: 1,
-        },
-        {
-          winner: 'Jogador 25',
-          match: 1,
-        },
-        {
-          winner: 'Jogador 27',
-          match: 1,
-        },
-      ];
+  data() {
+    return {
+      matches: [],
+    };
+  },
 
   methods: {
     backToHomePage() {
