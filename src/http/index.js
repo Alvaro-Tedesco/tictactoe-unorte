8646ba2561a66557da14db131fddf9a72b9a47bb
Alvaro Tedesco <alv.ted@outlook.com>
2023-10-15 12:56:11 -0300

    refactor: Removed unnecessary comment and adjusted the port of the url connecting to the server.

--- src/http/index.js
+++ src/http/index.js
@@ -1,8 +1,7 @@
 import axios from "axios";
 import services from "./services";
 
-/** @type {AxiosInstance} */
-const createdAxios = axios.create({baseURL: "http://localhost:5000/maquina-velha/"});
+const createdAxios = axios.create({baseURL: "http://localhost:5000/maquina-velha/",});
 
 const configs = JSON.parse(JSON.stringify(services));
 
