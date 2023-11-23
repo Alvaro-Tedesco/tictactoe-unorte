7ab38946bcb38aa3ac6dd6ad026a2d44a757ce9e
Alvaro Tedesco <alv.ted@outlook.com>
2023-11-23 14:22:05 -0300

    Feat: Added page not found for routes outside mapped routes.

--- src/router/routes.js
+++ src/router/routes.js
@@ -1,7 +1,13 @@
-import {routes as gameRoom} from "../views/game_room";
 import {routes as home} from "../views/home";
+import {routes as gameRoom} from "../views/game_room";
+import {routes as notFound} from "../views/not_found";
 
 export default [
-  ...gameRoom,
   ...home,
+  ...gameRoom,
+  ...notFound,
+  {
+    path: "*",
+    redirect: {name: "not_found",},
+  },
 ];
---
+++ src/views/not_found/pages/NotFoundPage.vue
@@ -0,0 +1,26 @@
+<template>
+  <section class="root-not-found-page w-full min-h-screen flex justify-center items-center">
+    <div>
+      <img
+          width="300"
+          height="300"
+          class="m-auto rounded-full"
+          alt="404 página não encontrada!"
+          src="../../../assets/images/404-icon.jpeg">
+
+      <h2 class="pt-4 text-white text-3xl font-bold">404 - PÁGINA NÃO ENCONTRADA</h2>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "NotFoundPage",
+};
+</script>
+
+<style lang="scss" scoped>
+.root-not-found-page {
+  background-image: linear-gradient(to bottom, #F25835, #000000 130%);
+}
+</style>
