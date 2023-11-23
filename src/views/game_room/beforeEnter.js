c1ff0d96bc1eecbd37f61ab54b896ffaa72131c8
Alvaro Tedesco <alv.ted@outlook.com>
2023-11-23 19:20:46 -0300

    Feat: Added check that validates route data.

---
+++ src/views/game_room/beforeEnter.js
@@ -0,0 +1,19 @@
+import Player from "../../enums/Player";
+
+export default (to, from, next) => {
+  const playersId = {
+    [Player.PLAYER_1.id]: Player.PLAYER_1.value,
+    [Player.PLAYER_2.id]: Player.PLAYER_2.value,
+    [Player.SPECTATOR.id]: Player.SPECTATOR.value,
+  };
+
+  console.log(to)
+
+  if (!playersId[to.params.playerId]
+    || (to.name === "game_board" && (to.params.replay !== "true" && to.params.replay !== ""))
+  ) {
+    next({name: "not_found",});
+  } else {
+    next();
+  }
+};
