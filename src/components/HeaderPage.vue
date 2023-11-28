e03656dbd0b6265b313f3532834b87ec446f36aa
Alvaro Tedesco <alv.ted@outlook.com>
2023-11-28 20:17:13 -0300

    Style: Adjustment to enlarge the logos in the HeaderPage.vue component.

--- src/components/HeaderPage.vue
+++ src/components/HeaderPage.vue
@@ -1,25 +1,27 @@
 <template>
   <section class="root-header-page">
-    <nav class="w-full flex justify-evenly items-center">
-      <div class="w-1/3 flex flex-wrap justify-around">
-        <img class="w-[70px] min-h-[82px]" src="../assets/images/UNORTE_BRANCO.svg" alt="Logo UNORTE">
-        <img class="w-[70px] min-h-[82px]" src="../assets/images/LAMPI_BRANCO.svg" alt="Logo LAMPI">
-        <img class="w-[70px] min-h-[82px]" src="../assets/images/LER_BRANCO.svg" alt="Logo LER">
+    <nav class="w-full flex items-center">
+      <div class="w-2/5 flex flex-wrap justify-around">
+        <img class="w-[180px] min-h-[82px]" src="../assets/images/UNORTE_BRANCO.svg" alt="Logo UNORTE">
+        <img class="w-[150px] min-h-[82px]" src="../assets/images/LAMPI_BRANCO.svg" alt="Logo LAMPI">
+        <img class="w-[80px] min-h-[82px]" src="../assets/images/LER_BRANCO.svg" alt="Logo LER">
         <img class="w-[70px] min-h-[82px]" src="../assets/images/NIC_BRANCO.svg" alt="Logo NIC">
       </div>
 
-      <div class="w-1/3 flex justify-center">
-        <label class="text-white font-bold text-xl">Jogo da Velha</label>
-      </div>
+      <div class="w-3/5 px-16 flex flex-wrap justify-between">
+        <div class=" flex justify-center">
+          <label class="text-white font-bold text-xl">Jogo da Velha</label>
+        </div>
 
-      <div class="w-1/3 flex flex-wrap justify-center gap-8">
-        <router-link :to="{name: 'home',}">
-          <span>Página inicial</span>
-        </router-link>
+        <div class=" flex flex-wrap justify-center gap-8">
+          <router-link :to="{name: 'home',}">
+            <span>Página inicial</span>
+          </router-link>
 
-        <router-link :to="{name: 'matches',}">
-          <span>Partidas</span>
-        </router-link>
+          <router-link :to="{name: 'matches',}">
+            <span>Partidas</span>
+          </router-link>
+        </div>
       </div>
     </nav>
   </section>
