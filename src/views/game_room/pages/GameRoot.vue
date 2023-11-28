009008756715e5a931dc123deecd279a1fc94222
Alvaro Tedesco <alv.ted@outlook.com>
2023-11-28 20:15:14 -0300

    Style: Adjust the style to enlarge logos for larger screens.

--- src/views/game_room/pages/GameRoot.vue
+++ src/views/game_room/pages/GameRoot.vue
@@ -1,19 +1,28 @@
 <template>
   <section class="root-game w-full">
     <header class="w-full">
-      <div class="w-full pt-4 flex flex-wrap justify-center gap-8">
-        <img class="w-[70px] min-h-[50px]" src="../../../assets/images/UNORTE_BRANCO.svg" alt="Logo UNORTE">
-        <img class="w-[70px] min-h-[50px]" src="../../../assets/images/LAMPI_BRANCO.svg" alt="Logo LAMPI">
-        <img class="w-[70px] min-h-[50px]" src="../../../assets/images/LER_BRANCO.svg" alt="Logo LER">
-        <img class="w-[70px] min-h-[50px]" src="../../../assets/images/NIC_BRANCO.svg" alt="Logo NIC">
+      <div class="sm:hidden w-full p-4 flex flex-wrap justify-center gap-8">
+        <img class="w-[220px] min-h-[50px]" src="../../../assets/images/UNORTE_BRANCO.svg" alt="Logo UNORTE">
+        <img class="w-[190px] min-h-[50px]" src="../../../assets/images/LAMPI_BRANCO.svg" alt="Logo LAMPI">
+        <img class="w-[120px] min-h-[50px]" src="../../../assets/images/LER_BRANCO.svg" alt="Logo LER">
+        <img class="w-[110px] min-h-[50px]" src="../../../assets/images/NIC_BRANCO.svg" alt="Logo NIC">
       </div>
 
-      <h1 class="pt-6 text-white font-bold text-2xl text-center">Jogo da Velha</h1>
+      <h1 class="pt-4 text-white font-bold text-2xl text-center">Jogo da Velha</h1>
+
+      <h5 class="w-full pt-4 text-white text-center">ROBÓTICA COLABORATIVA</h5>
     </header>
 
     <main class="w-full">
       <router-view/>
     </main>
+
+    <div class="hidden sm:w-full sm:p-4 sm:pb-12 sm:flex sm:flex-wrap sm:justify-center sm:gap-8">
+      <img class="w-[250px] min-h-[50px]" src="../../../assets/images/UNORTE_BRANCO.svg" alt="Logo UNORTE">
+      <img class="w-[210px] min-h-[50px]" src="../../../assets/images/LAMPI_BRANCO.svg" alt="Logo LAMPI">
+      <img class="w-[150px] min-h-[50px]" src="../../../assets/images/LER_BRANCO.svg" alt="Logo LER">
+      <img class="w-[140px] min-h-[50px]" src="../../../assets/images/NIC_BRANCO.svg" alt="Logo NIC">
+    </div>
   </section>
 </template>
 
@@ -74,7 +83,15 @@ export default {
   background-image: linear-gradient(to bottom, #F25835, #000000 130%);
 
   main {
-    min-height: calc(100vh - 122px);
+    min-height: calc(100vh - 48px);
+  }
+}
+
+@media (min-width: 640px) {
+  .root-game {
+    main {
+      min-height: calc(100vh - 242px);
+    }
   }
 }
 </style>
